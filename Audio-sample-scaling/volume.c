780730023f0e0c097aed15eb66ba8b0ae2dfd5ce
Ghaith-amdouni <ghaith.amdouni@insat.ucar.tn>
2025-04-02 14:21:11 +0100

    Add files via upload

---
+++ Audio-sample-scaling/volume.c
@@ -0,0 +1,42 @@
+// Modifies the volume of an audio file
+// scaling the sample
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+    FILE *input = fopen(argv[1], "rb");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "wb");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+    uint8_t header[HEADER_SIZE];
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+    int16_t sample;
+    while (fread(&sample, sizeof(int16_t), 1, input))
+    {
+        sample = (int16_t)(sample * factor);
+        fwrite(&sample, sizeof(int16_t), 1, output);
+    }
+    fclose(input);
+    fclose(output);
+    return 0;
+}
